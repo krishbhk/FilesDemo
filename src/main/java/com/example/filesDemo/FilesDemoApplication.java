72917e60d134b4afd85e5c4cee88a6e134e83a0a
rishkola <rishabh.kumar2@olacabs.com>
2022-09-12 11:51:33 +0530

    Initialized

---
+++ src/main/java/com/example/filesDemo/FilesDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.filesDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilesDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilesDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/filesDemo/FilesDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.filesDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilesDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
